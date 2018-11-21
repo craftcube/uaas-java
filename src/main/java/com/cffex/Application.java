56607f47696cf37034faf1fa7440518500450292
chenm <chenm@chenmsMacBookPro.local>
2018-11-21 14:27:31 +0800

    make changes

--- src/main/java/com/cffex/Application.java
+++ src/main/java/com/cffex/Application.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) throws Exception {
